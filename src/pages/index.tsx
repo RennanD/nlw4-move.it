4e738e9f08aa41d061555588e0a110269132ac10
rennand <rplaykl@gmail.com>
2021-02-23 23:04:55 -0300

    feat: Init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "next/head"
+import { ExperienceBar } from "../components/ExperienceBar"
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
